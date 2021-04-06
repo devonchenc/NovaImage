636977d6f7acd1e8f279d7a53c53a73c2cb02a80
Devon <devonchenc@gmail.com>
2021-04-06 15:22:35 +0800

    Edit mainwindow.h

--- Core/mainwindow.h
+++ Core/mainwindow.h
@@ -33,7 +33,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget* parent=nullptr);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
 public:
